bd8d1895493252cbd2c66172752cba179997c85b
gabor-farkas <gabor.farkas@doctusoft.com>
2016-06-28 14:13:39 +0200

    method descriptors added

--- src/typescript-rtti.ts
+++ src/typescript-rtti.ts
@@ -1,10 +1,26 @@
 declare module 'typescript-rtti' {
-    export interface PropertyDescriptor {
-        name: string,
-        type: 'string' | 'number' | 'object',
+
+    export interface TypeDescriptor {
+        name: 'string' | 'number' | 'object',
         reflect?: any,
         constructor?: any,
         array?: boolean,
         enum?: boolean
+    }
+
+    export interface PropertyDescriptor {
+        name: string,
+        type: TypeDescriptor;
+    }
+
+    export interface ParameterDescriptor {
+        name: string;
+        type: TypeDescriptor;
+    }
+
+    export interface MethodDescriptor {
+        name: string,
+        returnType: TypeDescriptor,
+        parameters: ParameterDescriptor[]
     }
 }
