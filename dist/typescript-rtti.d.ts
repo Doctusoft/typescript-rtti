b461c3e015ecaf5b6ede9059b5767d4bfe34aba4
gabor-farkas <gabor.farkas@doctusoft.com>
2016-06-28 15:48:48 +0200

    dist

--- dist/typescript-rtti.d.ts
+++ dist/typescript-rtti.d.ts
@@ -1,10 +1,22 @@
 declare module 'typescript-rtti' {
-    interface PropertyDescriptor {
-        name: string;
-        type: 'string' | 'number' | 'object';
+    interface TypeDescriptor {
+        name: 'string' | 'number' | 'object';
         reflect?: any;
         constructor?: any;
         array?: boolean;
         enum?: boolean;
+    }
+    interface PropertyDescriptor {
+        name: string;
+        type: TypeDescriptor;
+    }
+    interface ParameterDescriptor {
+        name: string;
+        type: TypeDescriptor;
+    }
+    interface MethodDescriptor {
+        name: string;
+        returnType: TypeDescriptor;
+        parameters: ParameterDescriptor[];
     }
 }
